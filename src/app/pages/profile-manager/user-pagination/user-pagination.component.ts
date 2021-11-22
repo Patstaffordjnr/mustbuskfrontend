57f0f190a82f585a9f1b963f5b3c77c07aecec7a
Patrick Stafford <Patrick Stafford>
2021-11-22 15:32:11 +0000

    Pagination displayed but not sync

--- src/app/pages/profile-manager/user-pagination/user-pagination.component.ts
+++ src/app/pages/profile-manager/user-pagination/user-pagination.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { User } from '../../../services/user/User';
 import { UserService } from 'src/app/services/user/user.service';
 import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-user-pagination',
@@ -12,21 +13,42 @@ import { HttpClient } from '@angular/common/http';
 
 export class UserPaginationComponent {
 
-  constructor(private http: HttpClient, private userServce: UserService) { }
+  
+  
+  constructor(private http: HttpClient, private userServce: UserService, private changeDetection: ChangeDetectorRef) { }
 
+pageNumber = 0;
+itemsPerPage = 10;
+users = this.userServce.getUsers();
+listOfUsers = []
 
+  async ngOnInit() {
+    let listOfUsers = await this.userServce.getUsers();
 
-  async getAll() {
-
-    // debugger;
-    let allUsers = await this.userServce.getUsers();
-
-    console.log(allUsers);
-
-
-
+    for (let i = this.pageNumber; i < this.itemsPerPage; i ++) {
+      
+      this.listOfUsers.push(listOfUsers[i]);
+      console.log(this.listOfUsers[i]);
+      this.changeDetection.detectChanges();
+    };
   }
 
+hupYaBoyo() {
+  this.listOfUsers.length = 0
+  this.pageNumber = this.pageNumber + 10;
+  console.log(this.pageNumber);
+  this.changeDetection.detectChanges();
+  this.ngOnInit()
+
+}
+
+downYaBoyo() {
+  this.pageNumber = this.pageNumber - 10;
+  console.log(this.pageNumber);
+  this.changeDetection.detectChanges();
+}
+}
+  
 //   getAll() {
 //     return this.http.get<User[]>(`${environment.site}${environment.apiUrl}home`);
 // }
@@ -38,4 +60,17 @@ export class UserPaginationComponent {
 //     } 
 //     return roleNames;
 // }
-}
+
+
+  // async getAll() {
+
+  //   let allUsers = await this.userServce.getUsers();
+  //   console.log(allUsers);
+
+  //   for (let i = 0; i < allUsers.length; i ++) {
+  //     console.log(allUsers[i]);
+  //     this.listOfUsers.push(allUsers[i]);
+  //   };
+  //   return console.log(this.listOfUsers)
+  // }
+
