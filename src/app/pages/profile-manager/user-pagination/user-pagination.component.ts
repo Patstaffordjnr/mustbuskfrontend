b39ad20767f7dbe0db43a285866b06de50b841ad
Patrick Stafford <Patrick Stafford>
2021-11-23 16:06:32 +0000

    Pagination core object complete

--- src/app/pages/profile-manager/user-pagination/user-pagination.component.ts
+++ src/app/pages/profile-manager/user-pagination/user-pagination.component.ts
@@ -3,31 +3,38 @@ import { User } from '../../../services/user/User';
 import { UserService } from 'src/app/services/user/user.service';
 import { HttpClient } from '@angular/common/http';
 
-
 @Component({
   selector: 'app-user-pagination',
   templateUrl: './user-pagination.component.html',
   styleUrls: ['./user-pagination.component.css']
 })
 
-
 export class UserPaginationComponent {
 
-  
-  
   constructor(private http: HttpClient, private userServce: UserService, private changeDetection: ChangeDetectorRef) { }
 
 pageNumber = 1;
 itemsPerPage = 10;
 users = this.userServce.getUsers();
-listOfUsers = []
+listOfUsers = [];
+currentPageNumber = [this.pageNumber];
+contentPageNumbers = [];
 
   async ngOnInit() {
-    let alistOfUsers = await this.userServce.getUsers();
-    for (let i = 0; i < alistOfUsers.length; i ++) {
-      this.listOfUsers.push(alistOfUsers[i]);
-
+    let aListOfUsers = await this.userServce.getUsers();
+    for (let i = 0; i < aListOfUsers.length; i ++) {
+      this.listOfUsers.push(aListOfUsers[i]);
     };
+
+     this.contentPageNumbers.length = 10;
+     let listOfNumbers = Math.ceil(this.listOfUsers.length / this.contentPageNumbers.length);
+
+     for (let i = 0; i < listOfNumbers; i ++) {
+      this.contentPageNumbers.push({i});
+    };
+    this.contentPageNumbers.length = this.contentPageNumbers.length - 9;
+
+
   }
 
 async hupYaBoyo() {
@@ -36,6 +43,16 @@ async hupYaBoyo() {
 async downYaBoyo() {
   this.pageNumber = this.pageNumber - 1;
 }
+
+
+selectedPageNumber?: Number;
+onSelect(selectedPage: Number): Number {
+console.log(selectedPage);
+this.pageNumber = Number(selectedPage);
+  return selectedPage
+  
+}
+
 
 }
   
